743d1607c28c05fbe7eb4ba74b854e0eff80709f
ohsam <end2000n@naver.com>
2020-12-19 22:00:36 +0900

    add TodoTemplate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -10,7 +11,7 @@ function App() {
 	return (
 		<>
 			<GlobalStyle />
-			<div>안녕하세요!</div>
+			<TodoTemplate>안녕하세요!</TodoTemplate>
 		</>
 	);
 }
---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const TodoTemplateBlock = styled.div`
+	position: relative;
+	margin: 96px auto 32px;
+	display: flex;
+	flex-direction: column;
+	width: 512px;
+	height: 768px;
+	background-color: #fff;
+	border-radius: 16px;
+	box-shadow: 0 0 8px rgba(0, 0, 0, 0.04);
+`;
+
+function TodoTemplate({ children }) {
+	return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+}
+
+export default TodoTemplate;
